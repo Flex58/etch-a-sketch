d9e0a33c14e0b44df5d09fd653fcc02b71262014
Flex58 <164533598+Flex58@users.noreply.github.com>
2024-05-13 13:31:05 +0200

    add mouseover eventListener that colors square

--- script.js
+++ script.js
@@ -1,19 +1,26 @@
 const container = document.querySelector("#container");
-let gridSize = 10;
+let gridSize = 50;
 let grid = [];
-let size =  800;
+const SIZE =  800;
 drawGrid()
-
+const gridSquare = document.querySelectorAll(".gridSquare");
+ 
 function drawGrid(){
     for (let i = 0; i < gridSize * gridSize; i++){
         grid[i] = container.appendChild(document.createElement("div")); 
         grid[i].style.width = 100 / gridSize  + "%";
         grid[i].style.height = 100 / gridSize  + "%"; 
-    }
+        grid[i].setAttribute("class", "gridSquare")
+     }
 }
 
-container.style.width =  size + "px";
-container.style.height =  size + "px"
+container.style.width =  SIZE + "px";
+container.style.height =  SIZE + "px"
 
 
-
+gridSquare.forEach((div) => {
+    div.addEventListener("mouseover", () => {
+        div.style.backgroundColor = "green";
+        console.log("test");
+    })
+})
