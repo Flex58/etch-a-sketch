69ea6006133e1460c8ca4ca8aa3446f168058596
Flex58 <164533598+Flex58@users.noreply.github.com>
2024-05-14 11:25:40 +0200

    add color selector and opacity fade

--- script.js
+++ script.js
@@ -11,12 +11,14 @@ function drawGrid(){
         grid[i] = container.appendChild(document.createElement("div")); 
         grid[i].style.width = 100 / gridSize  + "%";
         grid[i].style.height = 100 / gridSize  + "%"; 
-        grid[i].setAttribute("class", "gridSquare")
+        grid[i].setAttribute("class", "gridSquare");
      }
      gridSquare = document.querySelectorAll(".gridSquare");
      gridSquare.forEach((div) => {
+        div.style.opacity = 1;
         div.addEventListener("mouseover", () => {
-            div.style.backgroundColor = "green";
+            div.style.backgroundColor = document.querySelector("#color-select").value;
+            div.style.opacity -= 0.1;
         })
     })
     }
@@ -31,6 +33,7 @@ generate.addEventListener("click", () =>{
     for (let i = 0; i < gridSize * gridSize; i++){
         container.removeChild(grid[i]);
     }
+    container.style.backgroundColor = "black";
     do gridSize = prompt("Enter your Grid Size (cant exceed 100): ")
     while (gridSize > 100)
     drawGrid()
